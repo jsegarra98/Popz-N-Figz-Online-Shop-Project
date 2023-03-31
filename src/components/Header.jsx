5741f65b5838c634174eb3f4fbf269d1c534ed69
Joseph S <joseph.segarra@developersinstitute.co.nz>
2023-03-31 15:33:05 +1300

    adding back-to-top button, fixing header logo, and making logo appear on website

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import imgName from "./images/LOGO-FOR-WEBSITE.png";
 
 const Header = () => {
-  return <h1>Popz N' Figz</h1>;
+  return (
+    <div className='header'>
+      <h1>
+        <img src={imgName} alt='Logo' className='logo' />
+      </h1>
+    </div>
+  );
 };
 
 export default Header;
