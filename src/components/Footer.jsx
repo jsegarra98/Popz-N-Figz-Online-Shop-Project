74de4b8c8d6b81e6996c9597edba6fb857e4cc72
Joseph S <joseph.segarra@developersinstitute.co.nz>
2023-04-02 22:14:22 +1200

    fixing autograding problems

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 
 const Footer = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <footer>
-      <div className='back-to-top'>
-        <button>Back to top &#8593;</button>
+      <div>
+        <button className='backToTopButton' onClick={scrollToTop}>
+          Back to top &#8593;
+        </button>
       </div>
       <p class='copyright'>
         Copyright © 2023 Popz N' Figz, All rights reserved.
